1b360add9dd00cc0b3efa3ba2d9db41d99b2d55d
Farzin Mohammadi <farzinlize@live.com>
2017-01-11 17:01:01 +0330

    Branch show added
    ignore this commit

--- src/com/fuzzybunny/Main.java
+++ src/com/fuzzybunny/Main.java
@@ -11,6 +11,7 @@ import java.util.HashMap;
 public class Main {
 
 	public static void main(String[] arg) {
+		//in Master
 		Long time = System.currentTimeMillis();
 		File file = new File("DataSet\\XLarge-1\\input.txt");
 		try {
